ecee8309cff106ce28348f9cdaa4db164e064700
Chen-Zidi <czdpaige@gmail.com>
2021-05-09 00:10:14 +0200

    new api: return the POI which the user passes in the current run

---
+++ producer/src/main/java/com/runspec/producer/vo/POI.java
@@ -0,0 +1,55 @@
+package com.runspec.producer.vo;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document(collection = "POI")
+public class POI implements Serializable {
+    private String POIId;
+    private String name;
+    private String latitude;
+    private String longitude;
+
+    private double radius;
+
+    public String getPOIId() {
+        return POIId;
+    }
+
+    public void setPOIId(String POIId) {
+        this.POIId = POIId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
